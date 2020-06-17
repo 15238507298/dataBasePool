c268b99845bc645eb9f7c5f627419e7f52178748
lius <1986267850@qq.com>
2020-06-17 19:29:54 +0800

    2020-06-17 测试类添加日志打印类

--- src/com/lius/test/testPool.java
+++ src/com/lius/test/testPool.java
@@ -7,24 +7,23 @@ import java.sql.SQLException;
 import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
 
 import com.lius.connPool.connectionPool;
 
 public class testPool {
-	
 	private static AtomicInteger s = new AtomicInteger(0);
 	private static String propertyFilePath = "src/com/lius/resources/pool.properties";
 	private static int paralNums = 20000; 
 	private static CountDownLatch countDownMainLatch = new CountDownLatch(paralNums+1);
-
 	private static CountDownLatch countDownLatch = new CountDownLatch(paralNums);
 	private connectionPool connPool;
+	private static Logger logger = Logger.getLogger(Thread.currentThread().getName());
 	public static void main(String[] args) throws InterruptedException {
 		connectionPool connPool = connectionPool.getConnectionPool(propertyFilePath);
 //		connectionObj conn = connPool.getConnection().get();
-		
+
 		Thread.sleep(3000);
-		
 		for(int i =0;i<paralNums;i++) {
 			new Thread(()->{
 				try {
@@ -33,7 +32,7 @@ public class testPool {
 					query(connPool);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					logger.info(e.getMessage());
 				}
 				
 			}).start();
@@ -42,14 +41,12 @@ public class testPool {
 		countDownMainLatch.await();	
 	}
 	
-	
 	private static void query(connectionPool connPool) {
 		connPool.getConnection().ifPresent(conn->{
 			Connection connection = conn.getConn();
 			try {
 				PreparedStatement pre = connection.prepareStatement("select * from users");
 				ResultSet resultSet = pre.executeQuery();
-				
 				
 				while(resultSet.next()) {
 					System.out.println(String.format("[index:%d] id[%s],name[%s],password[%s]",
@@ -59,10 +56,9 @@ public class testPool {
 				connPool.reserveConnection(Optional.of(conn));
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				logger.info(e.getMessage());
 			}
 		});
 		countDownMainLatch.countDown();
-
 	}
 }
